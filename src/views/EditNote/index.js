ac7107b1c06c670992fe1ee889b1e088e2b33e93
wcfx <wcfx.dev@gmail.com>
2020-12-23 01:35:43 -0300

    Criando a estrutura para o bloco de notas

--- src/views/EditNote/index.js
+++ src/views/EditNote/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
-import { Container, ContainerImg, NotesImg } from './styles';
+import { Text } from 'react-native';
+import {
+  Container,
+  ContainerForm,
+  Scroller,
+  TitleInput,
+  BodyInput,
+  ContainerImg,
+  NotesImg,
+} from './styles';
 
 import notesImg from '../../assets/images/notes.png';
 
 const EditNotes = () => (
   <Container>
+    <ContainerForm>
+      <TitleInput />
+      <Scroller>
+        <BodyInput />
+      </Scroller>
+    </ContainerForm>
+
     <ContainerImg>
       <NotesImg source={notesImg} />
     </ContainerImg>
--- src/views/EditNote/styles.js
+++ src/views/EditNote/styles.js
@@ -5,6 +5,17 @@ export const Container = styled.View`
   background-color: #fff;
 `;
 
+export const ContainerForm = styled.View`
+  border: 3px solid red;
+  margin-top: 60px;
+  height: 400px;
+`;
+export const Scroller = styled.ScrollView``;
+
+export const TitleInput = styled.TextInput``;
+
+export const BodyInput = styled.TextInput``;
+
 export const ContainerImg = styled.View`
   flex: 1;
   justify-content: flex-end;
